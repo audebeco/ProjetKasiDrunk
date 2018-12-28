3805985721a562e7f94858b09e9f48dc1bf827bf
BOURDOISEAU <julien.bourdoiseau@sogeti.com>
2018-12-28 11:43:38 +0100

    commit

---
+++ app/src/main/java/com/example/caudeber/newyearapp/modules/Roue.java
@@ -0,0 +1,56 @@
+package com.example.caudeber.newyearapp.modules;
+
+public class Roue extends Thread {
+
+    interface WheelListener {
+        void newImage(int img);
+    }
+
+    private static int[] imgs = { };
+    public int currentIndex;
+    private WheelListener wheelListener;
+    private long frameDuration;
+    private long startIn;
+    private boolean isStarted;
+
+    public Roue(WheelListener wheelListener, long frameDuration, long startIn) {
+        this.wheelListener = wheelListener;
+        this.frameDuration = frameDuration;
+        this.startIn = startIn;
+        currentIndex = 0;
+        isStarted = true;
+    }
+
+    public void nextImg() {
+        currentIndex++;
+
+        if (currentIndex == imgs.length) {
+            currentIndex = 0;
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(startIn);
+        } catch (InterruptedException e) {
+        }
+
+        while(isStarted) {
+            try {
+                Thread.sleep(frameDuration);
+            } catch (InterruptedException e) {
+            }
+
+            nextImg();
+
+            if (wheelListener != null) {
+                wheelListener.newImage(imgs[currentIndex]);
+            }
+        }
+    }
+
+    public void stopWheel() {
+        isStarted = false;
+    }
+}
