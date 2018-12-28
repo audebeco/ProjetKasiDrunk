1e8de5a214b8db43c030f93de9eb2457c2bb5af6
Corentin Audebert <corentin.audebert@gmail.com>
2018-12-28 11:29:23 +0100

    ajout du bouton en avant qui ouvre la rouletteActivity

--- app/src/main/java/com/example/caudeber/newyearapp/MainActivity.java
+++ app/src/main/java/com/example/caudeber/newyearapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.caudeber.newyearapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView btStart = findViewById(R.id.btStart);
+        btStart.setOnClickListener(this::onClick);
+    }
+
+
+
+    private void onClick(View v){
+        startActivity(RouletteActivity.getStartIntent(this));
     }
 }
--- app/src/main/java/com/example/caudeber/newyearapp/RouletteActivity.java
+++ app/src/main/java/com/example/caudeber/newyearapp/RouletteActivity.java
@@ -1,5 +1,7 @@
 package com.example.caudeber.newyearapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,8 @@ public class RouletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_roulette);
+    }
+    public static  Intent getStartIntent(final Context ctx){
+        return new Intent(ctx,RouletteActivity.class);
     }
 }
